038f938e282e426f0318c184ab7bedd82633662d
Grittie <grittielars@gmail.com>
2020-02-11 14:17:32 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,7 @@
+var h1 = document.querySelector("h1");
+h1.classList.add("title");
+
+var italic = document.querySelectorAll("p");
+italic.forEach(function(element){
+    element.classList.add('italic');
+});;
